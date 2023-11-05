f369a696f0a110f350b94ff4963af109f7396bc6
Haldo Somoza <haldo_somoza@yahoo.com>
2023-11-05 18:27:12 -0500

    first commit

---
+++ AssignmentDay01/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class Fibonacci
+    {
+
+        // Get a list with the Fibonacci Series given the num of terms parameter.
+        public List<long> GetSeries(int num_terms)
+        {
+            // creating one empty resulting list
+            var series = new List<long>();
+
+            // looping from 0 to num_terms-1 ...
+            for (int i = 0; i < num_terms; i++)
+            {
+                // if value 0 or 1, add the number
+                if (i == 0 || i == 1) series.Add(i);
+                // else, add the sum of two previous added numbers
+                else series.Add(series[i-2]+series[i-1]);
+            }
+
+            // returning the resulting list
+            return series;
+        }
+
+    }
+}
---
+++ AssignmentDay01/Program.cs
@@ -0,0 +1,42 @@
+﻿using Assignments;
+
+Console.WriteLine("= = = = = Console App to Get Fibonacci Series = = = = =\n");
+
+// reading the number of terms
+Console.Write("Enter the Number of Terms in the Fibonacci Series: ");
+string? str_terms = Console.ReadLine();
+if (int.TryParse(str_terms, out int int_terms))
+{
+    if (int_terms <  1) Console.WriteLine("\nERROR: Number of Terms must be 1 or greater.");
+    else 
+    if (int_terms > 90) Console.WriteLine("\nERROR: Number of Terms not supported, the value informed overload the results.");
+    else
+    {
+        // getting the Fibonacci Series
+        var fibonacci = new Fibonacci();
+        var fibseries = fibonacci.GetSeries(int_terms);
+
+        // printing the resulting series
+        bool firstPrinted = false;
+        Console.Write("\nFibonacci series up to 10 terms: ");
+        foreach (var i in fibseries)
+        {
+            if (firstPrinted)
+            {
+                Console.Write(", {0}", i);
+            }
+            else
+            {
+                Console.Write("{0}", i);
+                firstPrinted = true;
+            }
+        }
+        Console.WriteLine();
+    }
+} 
+else
+{
+    Console.WriteLine("\nERROR: Number of Terms must be integer.");
+}
+
+Console.WriteLine("\n= = = = = Execution Finished = = = = =\n");
